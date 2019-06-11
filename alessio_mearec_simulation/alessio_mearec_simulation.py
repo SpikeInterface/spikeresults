85448afcb364c097721cb14d5a202dbfa203788f
Samuel Garcia <sam.garcia.die@gmail.com>
2019-06-11 13:16:58 +0200

    Change with new "study" api

---
+++ alessio_mearec_simulation/alessio_mearec_simulation.py
@@ -0,0 +1,63 @@
+import time
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sn
+
+import spiketoolkit as st
+import spikeextractors as se
+from spiketoolkit.comparison import (setup_comparison_study, run_study_sorters,
+            aggregate_sorting_comparison, aggregate_performances_table)
+
+
+
+p = '/media/samuel/SamCNRS/DataSpikeSorting/mearec/'
+
+study_folder = p + 'study_mearec'
+
+
+def setup():
+    
+    mearec_filename = p + 'recordings_50cells_SqMEA-10-15um_60.0_10.0uV_27-03-2019_13_31.h5'
+
+    rec0  = se.MEArecRecordingExtractor(mearec_filename, locs_2d=False)
+    gt_sorting0 = se.MEArecSortingExtractor(mearec_filename)
+    for chan in rec0.get_channel_ids():
+        loc = rec0.get_channel_property(chan, 'location')
+        rec0.set_channel_property(chan, 'location', loc[1:])
+    
+    gt_dict = {'rec0' : (rec0, gt_sorting0) }
+    
+    setup_comparison_study(study_folder, gt_dict)
+    
+    
+    
+def run():
+    sorter_list = ['tridesclous', 'herdingspikes', ]   # 'mountainsort4' 
+    run_study_sorters(study_folder, sorter_list, mode='keep', engine='loop')
+
+def collect_results():
+    comparisons = aggregate_sorting_comparison(study_folder, exhaustive_gt=True)
+    dataframes = aggregate_performances_table(study_folder, exhaustive_gt=True)
+    
+    for (rec_name, sorter_name), comp in comparisons.items():
+        #~ comp = comparisons[('001_synth', 'tridesclous')]
+        print()
+        print(rec_name, sorter_name)
+        print(comp.count)
+        comp.print_summary()
+    
+    #~ plt.subplots()
+        comp.plot_confusion_matrix()
+    
+    
+    
+    plt.show()
+
+    
+if __name__ == '__main__':
+    #~ setup()
+    run()
+    
+    collect_results()
+    
