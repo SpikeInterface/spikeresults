e2892711146b40b49657a2c743a47c9ea4d8b8bc
Samuel Garcia <sam.garcia.die@gmail.com>
2019-06-18 17:21:33 +0200

    WIP

--- Spampinato_ground_thruth_252_channels/main_script.py
+++ Spampinato_ground_thruth_252_channels/main_script.py
@@ -12,10 +12,18 @@ import spiketoolkit as st
 import spikeextractors as se
 from spiketoolkit.comparison import (setup_comparison_study, run_study_sorters,
             aggregate_sorting_comparison, aggregate_performances_table)
+from spiketoolkit.comparison.groundtruthstudy import copy_sorting
+
+
+
+os.environ["IRONCLUST_PATH"] = '/home/samuel/smb4k/CRNLDATA/home/samuel.garcia/Documents/ironclust/'
+os.environ["KILOSORT_PATH"] = '/home/samuel/smb4k/CRNLDATA/home/samuel.garcia/Documents/KiloSort/'
+os.environ["NPY_MATLAB_PATH"] = '/home/samuel/smb4k/CRNLDATA/home/samuel.garcia/Documents/npy-matlab/'
+os.environ["KILOSORT2_PATH"] = '/home/samuel/smb4k/CRNLDATA/home/samuel.garcia/Documents/Kilosort2/'
 
 # my working path
 # basedir = '/media/samuel/SamCNRS/DataSpikeSorting/pierre/zenodo/'
-# basedir = '/media/samuel/dataspikesorting/DataSpikeSortingHD2/Pierre/zenodo/'
+#~ basedir = '/media/samuel/dataspikesorting/DataSpikeSortingHD2/Pierre/zenodo/'
 #~ basedir = '/mnt/data/sam/DataSpikeSorting/pierre_zenodo/'
 basedir = '/home/samuel/DataSpikeSorting/Pierre/zenodo/'
 
@@ -29,47 +37,18 @@ study_folder = basedir + 'study_gt252/'
 # ground truth information
 ground_truth_folder = basedir + 'ground_truth/'
 
-# file_list
-
-#~ all_rec_names = ['20170629_patch3', '20170728_patch2', '20170630_patch1', '20160426_patch2', '20170621_patch1',
-             #~ '20170627_patch1', '20170706_patch3', '20170706_patch1', '20170726_patch1', '20170725_patch1',
-             #~ '20160426_patch3', '20170622_patch1', '20170623_patch1', '20170622_patch2', '20170629_patch2',
-             #~ '20170713_patch1', '20160415_patch2', '20170706_patch2', '20170803_patch1']
-
-rec_names = [ '20160415_patch2', '20170803_patch1', '20170623_patch1', '20170622_patch1', '20160426_patch3', 
-            '20170725_patch1', '20170621_patch1', '20160426_patch2', '20170728_patch2']
-
-
-#~ rec_names = [ '20160415_patch2', '20170803_patch1',]
-# rec_names = [ '20160415_patch2', ]
-
-
-
-#~ bad = ['20170706_patch2', # noisy
-        #~ '20170713_patch1', # double peak
-        #~ '20170629_patch2', # near thresh
-        #~ '20170622_patch2', # too small
-        #~ '20170726_patch1', # near thresh
-        #~ '20170706_patch1', # near thresh
-        #~ '20170706_patch3', # near thresh
-        #~ '20170627_patch1', #near thresh
-        #~ '20170630_patch1', # double peak
-        #~ '20170629_patch3', #near thresh
-        #~ ]
-        
-        
-        
-        
-        
-
-
-
-# sorter list
-# sorter_list = ['tridesclous', 'mountainsort4', ]
-sorter_list = ['tridesclous']
 
 
 def setup_study():
+    rec_names = ['20160415_patch2',
+            '20170803_patch1',
+            '20160426_patch3', 
+            '20170725_patch1',
+            '20170621_patch1',
+            '20160426_patch2', 
+            '20170728_patch2',
+            '20170713_patch1',]
+    
     gt_dict = {}
     for rec_name in rec_names:
         
@@ -102,17 +81,16 @@ def setup_study():
         
 
 def run_all():
-    #~ sorter_list = ['tridesclous', 'herdingspikes', ]   # 'mountainsort4' 
-    #~ sorter_list = ['klusta', ]
-    sorter_list = ['tridesclous', ]
+    #~ sorter_list = ['tridesclous', 'herdingspikes', 'mountainsort4' ]
+    sorter_list = ['tridesclous']
     run_study_sorters(study_folder, sorter_list, mode='keep', engine='loop')
     
 
-
-
 def collect_results():
-    comparisons = aggregate_sorting_comparison(study_folder, exhaustive_gt=True)
-    dataframes = aggregate_performances_table(study_folder, exhaustive_gt=True)
+    #~ copy_sorting(study_folder)
+    
+    comparisons = aggregate_sorting_comparison(study_folder, exhaustive_gt=False)
+    dataframes = aggregate_performances_table(study_folder, exhaustive_gt=False)
     
     #~ for (rec_name, sorter_name), comp in comparisons.items():
         #~ print()
@@ -133,10 +111,15 @@ def collect_results():
 
 if __name__ == '__main__':
     #~ setup_study()
-    #~ run_all()
+    run_all()
     
     
-    collect_results()
+    #~ collect_results()
+    
+    
+
+    
+    
     
     
     
