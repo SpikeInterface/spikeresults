b5cb911e5ee14533cb7f43db03fc7887826e3f51
Alessio Buccino <alejoe9187@gmail.com>
2019-07-17 16:56:07 +0200

    updates to main_scripts to generate figures

--- MEArec_SqMEA1015/main_script.py
+++ MEArec_SqMEA1015/main_script.py
@@ -1,83 +1,124 @@
 import os
 
-
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 import spikeextractors as se
 from spiketoolkit.study import GroundTruthStudy
 import spikewidgets as sw
+from pathlib import Path
 
+plot_mearec = False
 
-p = './'
+def clean_plot(ax, label, sorters):
+    ax.set_xlabel('')
+    ax.set_ylabel(label, fontsize=20)
+    ax.yaxis.set_tick_params(labelsize=12)
+    ax.set_xticklabels(sorters, fontsize=15, rotation=45, ha='center')
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    fig = ax.get_figure()
+    fig.subplots_adjust(bottom=0.2)
+    fig.set_size_inches(8, 7)
 
+p = Path('.')
 
-study_folder = p + 'study_mearec_SqMEA1015um'
+study_folder = p / 'study_mearec_SqMEA1015um'
 
-mearec_filename = p + 'recordings_50cells_SqMEA-10-15um_60.0_10.0uV_27-03-2019_13-31.h5'
-rec0  = se.MEArecRecordingExtractor(mearec_filename)
+mearec_filename = p / 'recordings_50cells_SqMEA-10-15um_60.0_10.0uV_27-03-2019_13-31.h5'
+rec0 = se.MEArecRecordingExtractor(mearec_filename)
 gt_sorting0 = se.MEArecSortingExtractor(mearec_filename)
+study = None
+
+if plot_mearec:
+    import MEArec as mr
+    import MEAutility as mu
+
+    recgen = mr.load_recordings(mearec_filename)
+
+    # plot the probe
+    probe_info = recgen.info['electrodes']
+    probe_name = recgen.info['electrodes']['electrode_name']
+    mea = mu.return_mea(info=probe_info)
+    fig1, ax1 = plt.subplots()
+    mu.plot_probe(mea, ax=ax1)
+    ax1.axis('off')
+
+    fig2, axs = plt.subplots(nrows=3, ncols=4)
+    for i, unit_id in enumerate(gt_sorting0.get_unit_ids()[:12]):
+        ax = axs.flatten()[i]
+        mr.plot_templates(recgen, template_ids=unit_id, ax=ax)
+        ax.set_title('unit #{}'.format(unit_id))
+
+    fig3, ax3 = plt.subplots()
+    mr.plot_recordings(recgen, start_time=0, end_time=5, lw=0.1, ax=ax3)
 
 # Setup study folder
-if not os.path.isdir(study_folder):
+if not study_folder.is_dir():
     print('Setting up study folder:', study_folder)
     gt_dict = {'rec0': (rec0, gt_sorting0)}
-    GroundTruthStudy.setup(study_folder, gt_dict)
+    study = GroundTruthStudy.create(study_folder, gt_dict)
+
+if study is None:
+    study = GroundTruthStudy(study_folder)
 
 # Run sorters
-print('Running sorters')
-sorter_list = ['mountainsort4', 'spykingcircus', 'tridesclous']
-sorter_params = {'mountainsort4': {'adjacency_radius': 50}, 
-		 'spykingcircus': {'adjacency_radius': 50}}
-study = GroundTruthStudy(study_folder)
+sorter_list = ['herdingspikes', 'kilosort2', 'ironclust',
+               'spykingcircus', 'tridesclous']
+sorter_params = {'mountainsort4': {'adjacency_radius': 50},
+                 'spykingcircus': {'adjacency_radius': 50}}
 
-if not os.path.isdir(study_folder + '/sorter_folders'):
+if not (study_folder / 'sorter_folders').is_dir():
+    print('Running sorters')
     study.run_sorters(sorter_list, mode='keep', engine='loop')
-
 
 # Perform comparisons
 print('Performing comparisons and getting results')
-study.run_comparisons(exhaustive_gt=True)
+study.run_comparisons(exhaustive_gt=True, compute_misclassification=True)
 
 comparisons = study.comparisons
-dataframes = study.aggregate_dataframes(accuracy=0.9)
+dataframes = study.aggregate_dataframes(accuracy=0.8)
 
 # Plot confusion matrices
-fig1 = plt.figure()
+fig4 = plt.figure()
 for i, ((rec_name, sorter_name), comp) in enumerate(comparisons.items()):
     print(rec_name, sorter_name)
-    comp.print_summary()
+    comp.print_summary(accuracy=0.8)
 
-    ax = fig1.add_subplot(2, 3, i+1)
+    ax = fig4.add_subplot(2, 3, i + 1)
     sw.plot_confusion_matrix(comp, ax=ax, count_text=False, unit_ticks=False)
     ax.set_title(sorter_name)
 
 plt.show()
 
-count_units = dataframes['count_units']
-perf_units = dataframes['perf_by_units']
 run_times = dataframes['run_times']
+perf_units = dataframes['perf_by_units']
+perf_avg = dataframes['perf_pooled_with_average']
+count_units = dataframes['count_units']
 
+exclude_sorters = ['mountainsort4',  'tridesclous']
 
-fig2, ax = plt.subplots(2, 4)
-sns.barplot(data=run_times, x='sorter_name', y='run_time', ax=ax[0, 0])
+if len(exclude_sorters) > 0:
+    for name in exclude_sorters:
+        run_times = run_times[run_times['sorter_name'] != name]
+        perf_units = perf_units[perf_units['sorter_name'] != name]
+        perf_avg = perf_avg[perf_avg['sorter_name'] != name]
+        count_units = count_units[count_units['sorter_name'] != name]
 
-sns.swarmplot(data=perf_units, x='sorter_name', y='accuracy', ax=ax[0, 1])
-ax[0, 1].axhline(0.8, ls='--', color='red')
+sorters = run_times['sorter_name']
 
-sns.swarmplot(data=perf_units, x='sorter_name', y='precision', ax=ax[0, 2])
+fig5, ax5 = plt.subplots()
+ax5 = sns.barplot(data=dataframes['run_times'], x='sorter_name', y='run_time', ax=ax5, order=sorters)
+fig6, ax6 = plt.subplots()
+ax6 = sns.swarmplot(data=dataframes['perf_by_units'], x='sorter_name', y='accuracy', ax=ax6, order=sorters)
+fig7, ax7 = plt.subplots()
+ax7 = sns.barplot(data=dataframes['perf_pooled_with_average'], x='sorter_name', y='precision', ax=ax7, order=sorters)
+fig8, ax8 = plt.subplots()
+ax8 = sns.barplot(data=dataframes['count_units'], x='sorter_name', y='num_bad', ax=ax8, order=sorters)
 
-sns.swarmplot(data=perf_units, x='sorter_name', y='recall', ax=ax[0, 3])
+sorters = run_times['sorter_name']
 
-
-sns.barplot(data=count_units, x='sorter_name', y='num_well_detected', ax=ax[1, 0])
-ax[1, 0].axhline(50, ls='--', color='red')
-
-sns.barplot(data=count_units, x='sorter_name', y='num_false_positive', ax=ax[1, 1])
-ax[1, 1].axhline(50, ls='--', color='red')
-
-sns.barplot(data=count_units, x='sorter_name', y='num_redundant', ax=ax[1, 2])
-ax[1, 2].axhline(50, ls='--', color='red')
-
-sns.barplot(data=count_units, x='sorter_name', y='num_redundant', ax=ax[1, 3])
-ax[1, 3].axhline(50, ls='--', color='red')
+clean_plot(ax5, 'run times (s)', sorters)
+clean_plot(ax6, 'accuracy', sorters)
+clean_plot(ax7, 'precision', sorters)
+clean_plot(ax8, 'bad units', sorters)
--- Marques-Smith_rat_neuropixels/main_script.py
+++ Marques-Smith_rat_neuropixels/main_script.py
@@ -1,56 +1,115 @@
 import matplotlib.pyplot as plt
 import spikeextractors as se
 import spiketoolkit as st
+import pandas as pd
 import spikewidgets as sw
 import numpy as np
 import scipy
+import seaborn as sns
+from pathlib import Path
 import os
 
-p = './'
-results_folder = p + 'results/'
-working_folder = p + 'working2/'
+p = Path('.')
+results_folder = p / 'results'
+working_folder = p / 'working'
 
 if not os.path.isdir(results_folder):
     os.mkdir(results_folder)
 
-bin_file = p + 'c1/c1_npx_raw.bin'
+bin_file = p / 'c1' / 'c1_npx_raw.bin'
 numchan = 384
 dtype = 'int16'
 samplerate = 30000
 gain = 2.34
 
 # load channel locations
-chanMap = scipy.io.loadmat(p + 'chanMap.mat')
+chanMap = scipy.io.loadmat(p / 'chanMap.mat')
 geom = np.squeeze(np.array([chanMap['xcoords'], chanMap['ycoords']])).T
 
-if not os.path.isfile(results_folder + 'rec_filt.dat'):
+if not (working_folder / 'rec_filt.dat').is_file():
+    print('-----------------------------------------')
+    print('Preprocessing recording')
     recording = se.BinDatRecordingExtractor(datfile=bin_file, samplerate=samplerate,
                                             numchan=numchan, dtype=dtype, geom=geom,
                                             gain=gain)
     recording_filt = st.preprocessing.bandpass_filter(recording)
 
     # dump filtered data
-    se.write_binary_dat_format(recording=recording_filt, save_path=results_folder + 'rec_filt.dat',
+    se.write_binary_dat_format(recording=recording_filt, save_path=working_folder / 'rec_filt.dat',
                                dtype='float32', chunksize=20)
 
-recording_filt = se.BinDatRecordingExtractor(datfile=results_folder + 'rec_filt.dat', samplerate=samplerate,
+recording_filt = se.BinDatRecordingExtractor(datfile=working_folder / 'rec_filt.dat', samplerate=samplerate,
                                              numchan=numchan, dtype='float32', geom=geom)
 
 recording_cmr = st.preprocessing.common_reference(recording_filt, reference='median')
 
 rec_dict = {'rec': recording_cmr}
-
-
-sorter_list = ['tridesclous', 'spykingcircus']
-
+sorter_list = ['tridesclous', 'spykingcircus', 'mountainsort4', 'kilosort2', 'herdingspikes', 'ironclust']
 sorter_params = {'mountainsort4': {'adjacency_radius': 50},
                  'spyking_circus': {'adjacency_radius': 50}}
 
-result_dict = st.sorters.run_sorters(sorter_list=sorter_list, recording_dict_or_list=rec_dict, with_output=True,
-                                     debug=True, sorter_params=sorter_params, working_folder=working_folder)
+
+if not (results_folder / 'sortings').is_dir():
+    print('-----------------------------------------')
+    print('Running sorters')
+    result_dict = st.sorters.run_sorters(sorter_list=sorter_list, recording_dict_or_list=rec_dict, with_output=True,
+                                         debug=True, sorter_params=sorter_params, working_folder=working_folder)
+
+    print('-----------------------------------------')
+    print('Saving results')
+    for s in sorter_list:
+        sorting = result_dict[('rec', s)]
+        se.NpzSortingExtractor.write_sorting(sorting, results_folder / 'sortings' / str(s + '.npz'))
 
 print('-----------------------------------------')
-print('Saving results')
-for s in sorter_list:
-    sorting = result_dict['rec'][s]
-    se.NpzSortingExtractor.write_sorting(sorting, results_folder + s + '.npz')
+print('Loading sorting output')
+
+exclude_sorters = ['mountainsort4',  'tridesclous']
+sorting_dict = {}
+sortings = []
+sortings_names = []
+for s in (results_folder / 'sortings').iterdir():
+    if s.suffix == '.npz':
+        if s.stem not in exclude_sorters:
+            sort = se.NpzSortingExtractor(s)
+            sorting_dict[s.stem] = sort
+            sortings.append(sort)
+            sortings_names.append(s.stem)
+
+
+mcmp = st.comparison.compare_multiple_sorters(sorting_list=sortings, name_list=sortings_names, verbose=True)
+w = sw.plot_multicomp_graph(mcmp, draw_labels=False, colorbar=True, alpha_edges=0.2, node_cmap='rainbow')
+
+n_units = []
+sorters = []
+for (sname, s) in sorting_dict.items():
+    n_units.append(len(s.get_unit_ids()))
+    sorters.append(sname)
+    print(sname, len(s.get_unit_ids()))
+
+agreement_sortings = {}
+for i in range(1, len(sortings_names)):
+    sort_agreement = mcmp.get_agreement_sorting(minimum_matching=i+1)
+    agreement_sortings[i+1] = sort_agreement
+    n_units.append(len(sort_agreement.get_unit_ids()))
+    sorters.append('agr.'+str(i+1))
+    print('agr.', i+1, len(sort_agreement.get_unit_ids()))
+
+data = {'sorters': sorters, 'n_units': n_units}
+df = pd.DataFrame(data=data)
+
+ax = sns.barplot(x='sorters', y='n_units', data=df, orient=45)
+ax.set_xlabel('')
+ax.set_ylabel('Number of units', fontsize=20)
+ax.yaxis.set_tick_params(labelsize=12)
+ax.set_xticklabels(sorters, fontsize=15, rotation=45, ha='center')
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+
+groupedvalues = df.reset_index()
+
+for index, row in groupedvalues.iterrows():
+    ax.text(row.name, row.n_units + 2, int(row.n_units), color='black', ha="center")
+
+figsns = ax.get_figure()
+figsns.subplots_adjust(bottom=0.2)
